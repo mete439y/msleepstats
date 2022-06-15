e4ff51b370738bd44fc25a452ee4552281c8f2af
Tenaw Derseh <mete439@gmail.com>
2022-06-15 10:41:46 -0700

    gg

--- msleepStats.R
+++ msleepStats.R
@@ -3,21 +3,21 @@ suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(lm.beta))
 
 Q1 <- cor.test(msleep$sleep_total, msleep$bodywt)
-Q1
+
 
 Q2 <- select(msleep, sleep_total, sleep_rem,brainwt, bodywt)
-Q2
+
 
 Q3 <- lm(bodywt ~ vore, msleep)
-Q3
+
 
 Q4 <- lm(bodywt ~ vore + sleep_rem, msleep)
-summary(Q4)
+
 
 QQ <- msleep%>%
   filter(vore != "omni" & vore != "insecti")%>%
   mutate(vorebin = ifelse(vore == 'carni', 0, 1))
-QQ
+
 
 Q5 <- glm(formula = vore ~ sleep_total, family =  binomial(), data = QQ)
-summary(Q5)
+
